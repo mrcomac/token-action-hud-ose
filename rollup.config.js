da10e537d04ac4c0a52e4a2791480063a6b010a4
Marcos <marcos.rafael.machado@gmail.com>
2023-08-06 14:09:17 +0100

    fix action

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+
+export default [
+    {
+        input: 'scripts/token-action-hud-ose.js',
+        plugins: [
+            commonjs(),
+            resolve({ browser: true })
+        ],
+        output: {
+            format: 'esm',
+            file: 'dist/token-action-hud-ose.min.js',
+            generatedCode: { constBindings: true },
+            plugins: [
+                terser({ keep_classnames: true, keep_fnames: true })
+            ],
+            sourcemap: true
+        }
+    }
+]
